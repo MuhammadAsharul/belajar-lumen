c7a2a1972d421353dfeedfd8ceb0b92de0c0b2d2
MuhammadAsharul <masharul51@gmail.com>
2022-07-06 11:20:13 +0700

    belajar lumen hari pertama

---
+++ database/seeders/ProdukSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class ProdukSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('produk')->insert(
+            [
+                [
+                    'namaProduk' => 'Kopi',
+                    'deskripsiProduk' => 'Kopi merupakan minuman yang terbuat dari kopi arabika atau kopi robusta. Kopi ini biasanya dicampur dengan susu atau krim.',
+                    'hargaProduk' => 10000,
+                    'kategoriProduk' => 'minuman',
+                ],
+                [
+                    'namaProduk' => 'Ayam Pecel',
+                    'deskripsiProduk' => 'Ayam pecel adalah makanan yang terbuat dari daging ayam yang dicampur dengan bumbu-bumbu lainnya.',
+                    'hargaProduk' => 15000,
+                    'kategoriProduk' => 'makanan',
+                ]
+            ]
+        );
+    }
+}
