58bd5f7b78295777e97e13003f58584c092b5ec4
MuhammadAsharul <masharul51@gmail.com>
2022-07-07 10:38:38 +0700

    belajar lumen hari kedua

---
+++ app/Http/Controllers/ProdukController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produk;
+use Illuminate\Support\Facades\Validator;
+
+class ProdukController extends Controller
+{
+    //tampilkan data produk berupa json
+    public function index()
+    {
+        $produk = Produk::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Produk',
+            'data' => $produk
+        ], 200);
+    }
+
+    //proses tambah data produk di rest api
+    public function store(Request $request)
+    {
+        // validasi data
+        $validator = Validator::make($request->all(), [
+            'namaProduk' => 'required',
+            'deskripsiProduk' => 'required',
+            'hargaProduk' => 'required',
+            'kategoriProduk' => 'required'
+        ]);
+        // jika validasi gagal
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Semua kolom harus diisi',
+                'data' => $validator->errors()
+            ], 401);
+        }
+        // jika validasi berhasil
+        else {
+            $produk = Produk::create([
+                'namaProduk' => $request->input('namaProduk'),
+                'deskripsiProduk' => $request->input('deskripsiProduk'),
+                'hargaProduk' => $request->input('hargaProduk'),
+                'kategoriProduk' => $request->input('kategoriProduk')
+            ]);
+            if ($produk) {
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Data Produk berhasil ditambahkan',
+                    'data' => $produk
+                ], 201);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Data Produk gagal ditambahkan'
+                ], 400);
+            }
+        }
+    }
+}
